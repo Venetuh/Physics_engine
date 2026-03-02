a350f1060ee74665b2351e68d7a0a370c2e41014
Venetuh <blahblahmagnum@gmail.com>
2026-03-01 20:43:03 -0800

    unfortunately files were deleted but had this back up and have reordered it's structure to increase readability and portability. Projectile motion now correctly calculates time, and distance based on given parameters

--- Physics/include/Core/Physics.h
+++ Physics/include/Core/Physics.h
@@ -1,10 +1,14 @@
 #ifndef PHYSICSPROBLEM_H
 #define PHYSICSPROBLEM_H
 
-class PhysicsProblem {
+
+
+class Physics {
+protected:
+    double toRad(double deg);
 public:
     virtual void calculate() const = 0;  // pure virtual function
-    virtual ~PhysicsProblem() = default; // virtual destructor
+    virtual ~Physics() = default; // virtual destructor
 };
 
 #endif
